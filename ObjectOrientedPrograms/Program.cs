eff87bd81cb489b040889baa25eb762b9efa0577
Vallem Balu <vbnreddy5@gmail.com>
2021-08-17 12:45:47 +0530

    JSON Inventory Data Management

---
+++ ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace ObjectOrientedPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Object Oriented Program");
+        }
+    }
+}
